5b9daa43b319378709e8bb6d4d38f13dd708694b
jdrydn <james@jdrydn.com>
2023-10-24 19:03:45 +0100

    fix: Docker fixes

--- vue3-express-ssr-ts/install.sh
+++ vue3-express-ssr-ts/install.sh
@@ -4,6 +4,6 @@ set -ex
 npm --prefix ./src/backend ci
 npm --prefix ./src/frontend ci
 
-npm --prefix ./deploy/dev ci
+npm --prefix ./deploy/dev ci || true
 npm --prefix ./deploy/server ci
 npm --prefix ./deploy/aws-lambda ci
